4e24eb96b754b21f75e2837dcf9d441d6660f5e3
ryan-g13 <ryan.groesch@gmail.com>
2023-11-02 16:49:02 -0700

    Day 1 part 2

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
+
 
 class Post(BaseModel):
   title: str
@@ -11,18 +13,29 @@ class Post(BaseModel):
   published: bool = False
   rating: Optional[int] = None
 
-@app.get('/')
+my_posts = [
+  {"title": "My first post!", "content": "I should probably put content here", "published": True, "id": 1},
+  {"title": "I am a wizard", "content": "Harry Potter Ipsum", "published": False, "rating": 1, "id": 2},
+]
+
+@app.get("/")
 def root():
   return {"message": "Hola Senor"}
 
-@app.get('/posts')
+@app.get("/posts")
 def get_posts():
-  return {"data": "Posts should be here"}
+  return {"data": my_posts}
 
-@app.post('/posts')
+@app.post("/posts")
 def create_post(new_post: Post):
-  # title = new_post["title"]
-  # data = new_post["content"]
-  print(new_post.rating)
-  # return {"title": title, "data": data}
-  return {"Message": 'great success'}
+  post_dict = new_post.model_dump()
+  post_dict["id"] = randrange(2,4000000)
+  my_posts.append(post_dict)
+  return {"data": post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id):
+  for post in my_posts:
+    if int(id) == post["id"]:
+      return post
+  return {"message": f"No post with ID {id} found"}
