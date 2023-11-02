ae9b48942789003c48eabc123e735442d3bf3413
ryan-g13 <ryan.groesch@gmail.com>
2023-11-02 16:17:57 -0700

    Day one create posts route and get route set up

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Post(BaseModel):
+  title: str
+  content: str
+  published: bool = False
+  rating: Optional[int] = None
 
 @app.get('/')
 def root():
   return {"message": "Hola Senor"}
 
-@app.get('/posts/')
+@app.get('/posts')
 def get_posts():
   return {"data": "Posts should be here"}
+
+@app.post('/posts')
+def create_post(new_post: Post):
+  # title = new_post["title"]
+  # data = new_post["content"]
+  print(new_post.rating)
+  # return {"title": title, "data": data}
+  return {"Message": 'great success'}
