e0533a70dcf5f2804f60a9a316281effa1f99f06
ryan-g13 <ryan.groesch@gmail.com>
2024-01-28 17:48:43 -0800

    Votes added dynamically

--- app/main.py
+++ app/main.py
@@ -4,7 +4,8 @@ from .database import engine
 from .routers import post, user, auth, vote
 from .config import settings
 
-models.Base.metadata.create_all(bind=engine)
+# Prior to alembic this is necessary for initializing the db via postgres
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
